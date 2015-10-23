aeb4d9c180c63847886a64a72e3f37be50c20796
ttriche <tim.triche@gmail.com>
2015-10-23 16:14:11 -0700

    vectorized versions of intersect() and identical()

--- intersect.R
+++ intersect.R
@@ -3,10 +3,11 @@
 #' @param ...   As many intersect-able elements as you wish.
 #' @return      Their intersection (or an error, if they can't be intersected)
 #'
-#' @example intersectAll(letters[1:3], letters[3:6], letters[2:4])
+#' @example intersect(letters[1:3], letters[3:6], letters[2:4])
 #' 
 #' @seealso base::funprog 
+#' @seealso base::intersect
 #' 
 #' @export
 #' 
-intersectAll <- function(...) Reduce("intersect", list(...))
+intersect <- function(...) Reduce(base::intersect, list(...))
